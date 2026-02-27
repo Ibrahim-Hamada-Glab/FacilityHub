ba1471e7694dcda202546117c7c0186568da1389
ibrahim <glabhamada@gmail.com>
2026-02-27 20:05:46 +0200

    a

--- FacilityHub/FacilityHub.Services/Dtos/AuthDtos.cs
+++ FacilityHub/FacilityHub.Services/Dtos/AuthDtos.cs
@@ -41,6 +41,7 @@ namespace FacilityHub.Services.Dtos;
 
         [Required, DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+        
 
         [Required, DataType(DataType.Password), Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
